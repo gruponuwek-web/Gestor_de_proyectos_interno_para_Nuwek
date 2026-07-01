a8b01eecf3d3c66eb8a1d3a4bb8ac52ce0f795db
gruponuwek-web <gruponuwek@gmail.com>
2026-07-01 09:55:07 -0600

    cambios en vite

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/Gestor_de_proyectos_interno_para_Nuwek/",
+  build: {
+    outDir: "docs",
+  },
 });
